b998e9a9704ef2d8c294a43713f93f5ba78cd679
minsoft1115 <minsoft1115@naver.com>
2025-09-02 13:52:47 +0900

    add lite-xl & modify desktop exec

--- install/install-hypr.sh
+++ install/install-hypr.sh
@@ -10,6 +10,26 @@ add_line_if_not_exists() {
   fi
 }
 
+escape_sed_replacement() {
+  printf '%s' "$1" | sed -e 's/[\/&]/\\&/g'
+}
+
+replace_text() {
+  local file="$1"
+  local search="$2"
+  local replace="$3"
+
+  if [[ -f "$file" ]]; then
+    local esc_search=$(printf '%s' "$search" | sed -e 's/[\/&]/\\&/g')
+    local esc_replace=$(escape_sed_replacement "$replace")
+
+    sed -i "s/${esc_search}/${esc_replace}/g" "$file"
+    echo "Replaced all occurrences of '${search}' with '${replace}' in $file"
+  else
+    echo "File $file does not exist."
+  fi
+}
+
 mkdir $HOME/.config/minsoft1115/hypr
 
 cp ./hypr/hyprland-minsoft1115.conf $HOME/.config/minsoft1115/hypr
@@ -17,6 +37,14 @@ cp ./hypr/bindings-minsoft1115.conf $HOME/.config/minsoft1115/hypr
 cp ./scripts/power-menu.sh $HOME/.config/minsoft1115/scripts
 
 sudo pacman -S gedit --needed --noconfirm
+#yay -S neohtop
+sudo pacman -S lite-xl --needed --noconfirm
+
+cp /usr/share/applications/org.lite_xl.lite_xl.desktop $HOME/.local/share/applications/
+
+replace_text $HOME/.local/share/applications/org.lite_xl.lite_xl.desktop "Exec=lite-xl %F" "Exec=env GDK_SCALE=1 GDK_DPI_SCALE=1 lite-xl $HOME/Documents %F"
+
+#/usr/share/applications/org.lite_xl.lite_xl.desktop
 
 #systemctl --user stop hyprlock-suspend.service
 #systemctl --user disable hyprlock-suspend.service
