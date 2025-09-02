ec53ed4bd0d7026dab32905feeb152addd3b9e91
minsoft1115 <minsoft1115@naver.com>
2025-09-02 23:44:13 +0900

    use animated scrolling on lazyvim

--- install/install-lazyvim-plugins.sh
+++ install/install-lazyvim-plugins.sh
@@ -1,1 +1,3 @@
 cp ./nvim/scrollbar.lua $HOME/.config/nvim/lua/plugins/scrollbar.lua
+
+mv $HOME/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua $HOME/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua.bak
