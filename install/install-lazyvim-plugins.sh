e0dee489f1e02001f8b23cc77c09eecfcce95387
minsoft1115 <minsoft1115@naver.com>
2025-09-04 16:48:38 +0900

    lazyvim - modify diagnostic

--- install/install-lazyvim-plugins.sh
+++ install/install-lazyvim-plugins.sh
@@ -1,6 +1,7 @@
 dotnet tool install --global csharp-ls
 
 cp ./nvim/scrollbar.lua $HOME/.config/nvim/lua/plugins/
+cp ./nvim/diagnostics.lua $HOME/.config/nvim/lua/plugins/
 cp ./nvim/lsp.lua $HOME/.config/nvim/lua/plugins/
 
 mv $HOME/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua $HOME/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua.bak
---
+++ install/nvim/diagnostics.lua
@@ -0,0 +1,6 @@
+vim.diagnostic.config({
+  virtual_text = { current_line = true },
+  virtual_lines = { current_line = true }, -- 0.11+에서 지원
+  signs = false,
+  underline = true,
+})
