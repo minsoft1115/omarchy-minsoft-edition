5cfe38ea98ae009d1979be613c3ca78597f468df
minsoft1115 <minsoft1115@naver.com>
2025-09-08 08:30:17 +0900

    update lazyvim config

--- install/nvim/diagnostics.lua
+++ install/nvim/diagnostics.lua
@@ -1,6 +1,13 @@
-vim.diagnostic.config({
-  virtual_text = { current_line = true },
-  virtual_lines = { current_line = true }, -- 0.11+에서 지원
-  signs = false,
-  underline = true,
-})
+return {
+  {
+    "neovim/nvim-lspconfig", -- 플러그인 이름 필수
+    config = function()
+      vim.diagnostic.config({
+        virtual_text = { current_line = true },
+        virtual_lines = { current_line = true },
+        signs = false,
+        underline = true,
+      })
+    end,
+  },
+}
